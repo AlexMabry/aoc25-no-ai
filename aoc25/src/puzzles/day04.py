2a2b7f1a3161686e456d949fa0ebf0300d8c6280
Alex Mabry <mabryecom@gmail.com>
2025-12-06 15:53:06 -0600

    day 4

--- aoc25/src/puzzles/day04.py
+++ aoc25/src/puzzles/day04.py
@@ -1,15 +1,37 @@
 from ..utils import parse_data
 
+Locations = set[tuple[int, int]]
+
+
+def get_rolls(data: str) -> Locations:
+    grid = parse_data(data)
+    return {(x, y) for y, row in enumerate(grid) for x, c in enumerate(row) if c == "@"}
+
+
+def get_neighbors(x: int, y: int) -> Locations:
+    close = [-1, 0, 1]
+    return {(x + dx, y + dy) for dx in close for dy in close if dx != 0 or dy != 0}
+
+
+def rolls_to_remove(rolls: Locations) -> Locations:
+    return {roll for roll in rolls if len(get_neighbors(*roll) & rolls) < 4}
+
 
 def solve_part1(data: str):
-    input_data = parse_data(data)
-    print(input_data)
+    rolls = get_rolls(data)
+    to_remove = rolls_to_remove(rolls)
 
-    return None
+    return len(to_remove)
 
 
 def solve_part2(data: str):
-    input_data = parse_data(data)
-    print(input_data)
+    rolls = get_rolls(data)
+    to_remove = rolls_to_remove(rolls)
+    removed = set()
 
-    return None
+    while to_remove:
+        rolls = rolls - to_remove
+        removed = removed | to_remove
+        to_remove = rolls_to_remove(rolls)
+
+    return len(removed)
