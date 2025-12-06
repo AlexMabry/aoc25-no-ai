824e3b28560f7719a8d7e362f7e76d71f888aa2a
Alex Mabry <mabryecom@gmail.com>
2025-12-06 17:46:47 -0600

    day 6

--- aoc25/src/puzzles/day06.py
+++ aoc25/src/puzzles/day06.py
@@ -1,15 +1,29 @@
+from itertools import zip_longest
+
 from ..utils import parse_data
 
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    problems = [row.split() for row in input_data]
+    statements = (f" {p[-1]} ".join(p[:-1]) for p in zip(*problems))
 
-    return None
+    return sum(map(eval, statements))
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    print(input_data)
 
-    return None
+    answer = 0
+    numbers = []
+    for column in zip_longest(*map(reversed, input_data), fillvalue=""):
+        value = "".join(column[:-1]).strip()
+        if value:
+            numbers.append(value)
+
+        operator = column[-1].strip()
+        if operator:
+            answer += eval(f" {operator} ".join(numbers))
+            numbers = []
+
+    return answer
